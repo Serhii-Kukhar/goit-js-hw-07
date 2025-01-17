19941a47410c9d8c9f6818553fc397da578caed4
Serhii Kukhar <s3riykuhar1@gmail.com>
2025-01-17 19:43:57 +0100

    task4js

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,19 @@
+const registerForm = document.querySelector(".login-form");
 
+
+registerForm.addEventListener("submit", handleSubmit);
+
+function handleSubmit(e) {
+    e.preventDefault();
+    const form = e.target;
+    const email = form.elements.email.value;
+    const password = form.elements.password.value;
+    if (email === "" || password === "") {
+        alert(`All form fields must be filled in`);
+        return;
+    }
+        
+        console.log(`Email:${email},Password:${password}`);
+        form.reset();
+        
+    }
