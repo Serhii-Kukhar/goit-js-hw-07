89d28d6f29e7a1da5585a81970fdbf3a47af4c0b
Serhii Kukhar <s3riykuhar1@gmail.com>
2025-01-18 15:14:05 +0100

    task5

--- js/task-5.js
+++ js/task-5.js
@@ -2,4 +2,12 @@ function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
-}
+};
+
+const button = document.querySelector('.change-color');
+const colorSpan = document.querySelector('.color');
+
+button.addEventListener('click',()=> {
+document.body.style.backgroundColor = getRandomHexColor();
+colorSpan.textContent = getRandomHexColor();
+});
